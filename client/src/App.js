e62412e1e7ff60c8f5d91f788b3c805ba32a0dcb
Topeojewale <topeojewale@gmail.com>
2018-06-21 10:37:36 +0100

    Added banner component

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Navbar from '../src/components/navbar/Navbar'
+import Banner from '../src/components/banner/Banner'
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <Navbar/>
+        <Banner/>
       </div>
     );
   }
