5f738ab702e9102d0a91157a2a91fa02f156216f
Topeojewale <topeojewale@gmail.com>
2018-06-21 10:14:31 +0100

    initial commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,32 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-
-// function Hello() {
-//   return (
-//     <span>Hello {2+4} </span>
-//   )
-// }
-
-// function World() {
-//   return (
-//     <span>World</span>
-//   )
-// }
-
-// function HelloWorld() {
-//   return <div><Hello/> <World/></div>
-// };
-
-// const RenderUser = () => {
-//   let isLoggedIn = false;
-//   return (
-//     <div>{isLoggedIn? "Hello Tope":"User not found"}</div>
-//   )
-// }
-// ReactDOM.render(<RenderUser/>, document.getElementById('root'));
-// const notJSX = React.createElement('span', { className: 'hello' }, "Welcome Home");
-// ReactDOM.render(notJSX, document.getElementById("root"));
-
-
-
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
