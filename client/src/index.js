da0f55c49c508ced0df2104705cc1047fc048d5f
Topeojewale <topeojewale@gmail.com>
2018-06-21 09:59:17 +0100

    Initial project set up

---
+++ client/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+// function Hello() {
+//   return (
+//     <span>Hello {2+4} </span>
+//   )
+// }
+
+// function World() {
+//   return (
+//     <span>World</span>
+//   )
+// }
+
+// function HelloWorld() {
+//   return <div><Hello/> <World/></div>
+// };
+
+// const RenderUser = () => {
+//   let isLoggedIn = false;
+//   return (
+//     <div>{isLoggedIn? "Hello Tope":"User not found"}</div>
+//   )
+// }
+// ReactDOM.render(<RenderUser/>, document.getElementById('root'));
+// const notJSX = React.createElement('span', { className: 'hello' }, "Welcome Home");
+// ReactDOM.render(notJSX, document.getElementById("root"));
+
+
+
